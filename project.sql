f7dbbd4974884e56a03a9c5d27800d96b478f723
Dominic Zhang <dooooominic@gmail.com>
2026-04-21 14:07:41 -0400

    Initial commit

---
+++ project.sql
@@ -0,0 +1,25 @@
+
+
+/* 
+
+User: 
+  creates workspace 
+  creates channel 
+
+Workspace: 
+    contains channel 1:N
+
+Channel: 
+
+Message: 
+    user sends messages 
+    in channel/workspace 
+
+Invitation:
+    userid/inviter 
+    general invitation, no second userid needed
+
+
+
+*/
+
