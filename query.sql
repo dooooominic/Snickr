3714fbccf8c5583e5195f4e7b12731360eea4cb8
Dominic Zhang <dooooominic@gmail.com>
2026-04-22 16:35:52 -0400

    Updated queries for formatting

--- query.sql
+++ query.sql
@@ -14,7 +14,7 @@ WITH auth_check AS (
     SELECT 1
     FROM workspace_membership
     WHERE workspace_id = 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb' --uuid format
-      AND user_id      = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
+      AND user_id = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
 )
 INSERT INTO channels (workspace_id, channel_name, channel_type, creator_id)
 SELECT
@@ -28,7 +28,7 @@ WHERE EXISTS (SELECT 1 FROM auth_check);
 INSERT INTO channel_membership (channel_id, user_id)
 SELECT c.channel_id, c.creator_id
 FROM channels c
-WHERE c.creator_id   = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
+WHERE c.creator_id = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
   AND c.workspace_id = 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb'
   AND c.channel_name = 'general';
 
@@ -40,7 +40,7 @@ SELECT
     u.username,
 FROM workspaces w
 JOIN workspace_membership wm ON w.workspace_id = wm.workspace_id
-JOIN users u ON wm.user_id     = u.user_id
+JOIN users u ON wm.user_id = u.user_id
 WHERE wm.is_admin = true
 ORDER BY w.workspace_name, u.username;
 
@@ -51,16 +51,16 @@ SELECT
     COUNT(ci.invitee_user_id) AS invited_but_not_joined
 FROM channels c
 LEFT JOIN channel_invitation ci
-ON c.channel_id       = ci.channel_id
+ON c.channel_id = ci.channel_id
     AND ci.invite_status   = 'pending'
     AND ci.invitation_time < now() - INTERVAL '5 days'
     AND NOT EXISTS ( --users still not in channel
         SELECT 1
         FROM channel_membership cm
-        WHERE cm.channel_id = ci.channel_id AND cm.user_id    = ci.invitee_user_id
+        WHERE cm.channel_id = ci.channel_id AND cm.user_id = ci.invitee_user_id
           )
-WHERE c.workspace_id  = 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb'
-  AND c.channel_type  = 'public'
+WHERE c.workspace_id = 'bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb'
+  AND c.channel_type = 'public'
 GROUP BY c.channel_id, c.channel_name
 ORDER BY c.channel_name;
 
@@ -85,7 +85,7 @@ SELECT
     w.workspace_name,
     m.message_text
 FROM messages m
-JOIN channels c ON m.channel_id   = c.channel_id
+JOIN channels c ON m.channel_id = c.channel_id
 JOIN workspaces w ON c.workspace_id = w.workspace_id
 WHERE m.user_id = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
 ORDER BY m.message_time ASC;
@@ -99,9 +99,9 @@ SELECT
     c.channel_name,
     m.message_text
 FROM messages m
-JOIN channels c ON m.channel_id    = c.channel_id
-JOIN workspaces w ON c.workspace_id  = w.workspace_id
-JOIN users u_author ON m.user_id  = u_author.user_id
+JOIN channels c ON m.channel_id = c.channel_id
+JOIN workspaces w ON c.workspace_id = w.workspace_id
+JOIN users u_author ON m.user_id = u_author.user_id
 -- user must be a member of the channel
 JOIN channel_membership cm ON cm.channel_id = c.channel_id
 AND cm.user_id = 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa'
